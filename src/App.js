a0429a9c6be5c5bf06f5d559f28ed33f3ec0538b
RJHARENBERG <reinardharenberg@gmail.com>
2022-04-26 10:54:18 +0200

    stap-1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,44 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      Begin hier met de tutorial!
-    </div>
-  );
+    return (
+        <main>
+            <article className="product">
+                <img src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg" alt="Citroenen"/>
+                <h2 className="product-description">
+                    Citroen
+                </h2>
+                <p>
+                    Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het
+                    vruchtvlees,
+                    het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander
+                    voedsel.
+                </p>
+            </article>
+            <article className="product">
+                <img src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg" alt="Citroenen"/>
+                <h2 className="product-description">
+                    Limoen
+                </h2>
+                <p>
+                    Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten
+                    (Wijnruitfamilie).
+                    Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen.
+                </p>
+            </article>
+            <article className="product">
+                <img src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg" alt="Citroenen"/>
+                <h2 className="product-description">
+                    IJsblokjes
+                </h2>
+                <p>
+                    Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje. Het wordt gemaakt in
+                    een
+                    diepvriezer door water in een plastic vorm te laten bevriezen.
+                </p>
+            </article>
+        </main>
+    );
 }
 
 export default App;
